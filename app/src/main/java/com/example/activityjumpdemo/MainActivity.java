62b90e0d60a6c883719cbd9999446c2fc77d0903
zhujiafeng <zhujiafeng@xiaomi.com>
2025-09-05 16:24:22 +0800

    activity生命周期

---
+++ app/src/main/java/com/example/activityjumpdemo/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.activityjumpdemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        //给页面设置标题
+        //getSupportActionBar().setTitle("首页");
+
+
+
+    }
+
+    public void jumpTosetting(View view) {
+       // Intent intent=new Intent(MainActivity.this,SettingActivity.class);
+       // startActivity(intent);
+
+      //  Intent intent=new Intent();
+       // intent.setClass(this,SettingActivity.class);
+        //startActivity(intent);
+
+        Intent intent=new Intent();
+        intent.setClassName("com.example.activityjumpdemo","com.example.activityjumpdemo.SettingActivity");
+        startActivity(intent);
+    }
+}
